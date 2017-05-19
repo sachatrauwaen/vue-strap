0206145a9fbc1e39e8065fc9b2530d3c2245fc60
Sacha <sachatrauwaen@users.noreply.github.com>
2017-05-19 16:54:09 +0200

    Update index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,2 +1,17 @@
 // Simply make TS happy
-declare module 'vue-strap';
+
+declare module 'vue-strap' {
+    export class input { }
+    export class select { }
+    export class option { }
+    export class tooltip { }
+    export class popover { }
+    export class modal { }
+    export class tabs { }
+    export class tab { }
+    export class checkbox{}
+    export class button{}
+    export class alert{}
+    export class spinner{}
+    export class formValidator{}
+}
