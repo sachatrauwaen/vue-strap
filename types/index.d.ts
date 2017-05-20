fc36a7e490bf6ab0c35a122e27fb55f130434dd7
Sacha <sachatrauwaen@users.noreply.github.com>
2017-05-20 21:20:40 +0200

    typescript classes for vue strap components

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,17 +1,30 @@
-// Simply make TS happy
-
 declare module 'vue-strap' {
-    export class input { }
-    export class select { }
-    export class option { }
-    export class tooltip { }
-    export class popover { }
-    export class modal { }
-    export class tabs { }
-    export class tab { }
-    export class checkbox{}
-    export class button{}
-    export class alert{}
-    export class spinner{}
-    export class formValidator{}
+  export class accordion { }
+  export class affix { }
+  export class alert { }
+  export class aside { }
+  export class buttonGroup { }
+  export class carousel { }
+  export class checkbox { }
+  export class datepicker { }
+  export class dropdown { }
+  export class formGroup { }
+  export class formValidator { }
+  export class input { }
+  export class modal { }
+  export class navbar { }
+  export class option { }
+  export class panel { }
+  export class popover { }
+  export class progressbar { }
+  export class radio { }
+  export class select { }
+  export class slider { }
+  export class spinner { }
+  export class tab { }
+  export class tabGroup { }
+  export class tabs { }
+  export class toggleButton { }
+  export class tooltip { }
+  export class typeahead
 }
